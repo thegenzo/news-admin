0909aad26e2146fc5614d98ef8a6c5b7c1843ba3
Dicky Agusputra <loscarbulos@gmail.com>
2023-05-19 16:56:36 +0800

    adding package
    axios, js cookie, react chartjs 2, react confirm alert, react hot toast, reactjs pagination, react qull, react router dom and recoil (that's a lot packages hahaha)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+// BrowserRouter from react-router-dom
+import { BrowserRouter } from 'react-router-dom'
+// RecoilRoot from recoil for react state management
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />    
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
 )
